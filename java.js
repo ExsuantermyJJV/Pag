2d9fceb14c1085e7a174d0f52651cb448164a23c
ExsuantermyJJV <106329150+ExsuantermyJJV@users.noreply.github.com>
2022-09-30 03:13:23 -0500

    Update java.js

--- java.js
+++ java.js
@@ -22,3 +22,67 @@ mainDiv.addEventListener('click', function(){
   this.classList.toggle('open');
 });
 
+/* animate */
+
+const $pelota = document.getElementById('pelota');
+const animation = $pelota.animate([
+   {
+  transform: 'translateX(0)'
+  },
+  {
+    transform: 'translateY(100px)'
+  }
+]
+,{ marigin:0,
+  duration: 1000,
+  direction: 'alternate-reverse',
+  easing: 'linear',
+  iterations: 8,
+  fill: 'forwards',
+})
+
+const $playButton = document.getElementById('play');
+const $pauseButton = document.getElementById('pause');
+
+$playButton.addEventListener('click', (event) => {
+  animation.play();
+})
+$pauseButton.addEventListener('click', (event) => {
+  animation.pause();
+})
+
+
+/* spider */
+
+const $spi = document.getElementById('spi');
+const animation1 = $spi.animate(  
+  [
+ {
+   transform: 'translate(-10px) rotate(10deg) '
+   }
+     
+ ],
+ 
+ { 
+  duration: 1000,
+  direction: 'alternate-reverse',
+  easing: 'linear',
+  iterations: 9,
+  fill: 'forwards',
+  interationStart: .5, 
+})
+
+const $play1Button = document.getElementById('play1');
+const $pause1Button = document.getElementById('pause1');
+
+$play1Button.addEventListener('click', (event) => {
+  animation1.play();
+})
+$pause1Button.addEventListener('click', (event) => {
+  animation1.pause();
+})
+
+
+
+/* run */
+
